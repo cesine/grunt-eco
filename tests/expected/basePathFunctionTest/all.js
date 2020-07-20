0d2bcecd4b752587d192040f8f357fbe676f00e8
cesine <cesine@yahoo.com>
2020-07-20 09:54:59 -0400

    update expected compile ouput

--- tests/expected/basePathFunctionTest/all.js
+++ tests/expected/basePathFunctionTest/all.js
@@ -22,13 +22,13 @@ window.JST["advanced"] = function(__obj) {
       return _safe(result);
     };
     (function() {
-      var project, _i, _len, _ref;
+      var i, len, project, ref;
     
       if (this.projects.length) {
         _print(_safe('\n  '));
-        _ref = this.projects;
-        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-          project = _ref[_i];
+        ref = this.projects;
+        for (i = 0, len = ref.length; i < len; i++) {
+          project = ref[i];
           _print(_safe('\n    <a href="'));
           _print(project.url);
           _print(_safe('">'));
@@ -43,6 +43,51 @@ window.JST["advanced"] = function(__obj) {
       }
     
       _print(_safe('\n'));
+    
+    }).call(this);
+    
+    return __out.join('');
+  }).call((function() {
+    var obj = {
+      escape: function(value) {
+        return ('' + value)
+          .replace(/&/g, '&amp;')
+          .replace(/</g, '&lt;')
+          .replace(/>/g, '&gt;')
+          .replace(/"/g, '&quot;');
+      },
+      safe: _safe
+    }, key;
+    for (key in __obj) obj[key] = __obj[key];
+    return obj;
+  })());
+};
+
+if (!window.JST) {
+  window.JST = {};
+}
+window.JST["empty"] = function(__obj) {
+  var _safe = function(value) {
+    if (typeof value === 'undefined' && value == null)
+      value = '';
+    var result = new String(value);
+    result.ecoSafe = true;
+    return result;
+  };
+  return (function() {
+    var __out = [], __self = this, _print = function(value) {
+      if (typeof value !== 'undefined' && value != null)
+        __out.push(value.ecoSafe ? value : __self.escape(value));
+    }, _capture = function(callback) {
+      var out = __out, result;
+      __out = [];
+      callback.call(this);
+      result = __out.join('');
+      __out = out;
+      return _safe(result);
+    };
+    (function() {
+      _print(_safe('hi\n'));
     
     }).call(this);
     
